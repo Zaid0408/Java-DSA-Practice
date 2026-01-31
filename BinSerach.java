ca5486edaebc11698210d7fdec2e33da77779de3
Zaid <zaidhasan04081999@gmail.com>
2026-01-31 16:34:55 +0530

    M

--- BinSerach.java
+++ BinSerach.java
@@ -327,5 +327,55 @@ public class BinSerach {
 
         return -1;
     }
+    // leetcode 69 Sqrt(x) 
+    public int mySqrt(int x) {
+        if(x<=1)
+            return x;
+        int left=0,right=x;
+        int ans=0;
+        while(left<=right)
+        {
+            int mid=(left + right)/2;
+            if(mid<x/mid){
+                left=mid+1;
+                ans=mid; // could be possible max value whose sqaure <=x
+            }
+            else if(mid>x/mid) // better than mid*mid > x as the multiplication can cause overflow of integer and comparision will onot be possble 
+                right=mid-1;
+            else if(mid == x/mid)
+                return mid;   
+        }
+        return ans;
+    }
+
+    //nth root of a number
+    public int NthRoot(int N, int M) {
+        int s=0,e=M,mid=0,midN=0;
+        while(s<=e)
+        {
+            mid=(s+e)/2;
+            midN=func(mid,N,M);
+            if(midN== 1)
+                return mid;
+            else if(midN == 0)
+                s=mid+1;
+            else
+                e=mid-1;
+        }
+        return -1;
+    }
+    public int func(int mid, int n, int m)
+    {
+        long ans=1;
+        for(int i=1;i<=n;i++)
+        {
+            ans=ans*mid;
+            if(ans> m)
+                return 2;
+        }
+        if(ans==m)
+            return 1;
+        return 0;
+    }
 
 }
