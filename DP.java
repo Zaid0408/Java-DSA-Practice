8a955f24b2aabf803c9e1f4c8fb59eda0f287721
Zaid <zaidhasan04081999@gmail.com>
2026-08-22 13:24:37 +0530

    DP revision

--- DP.java
+++ DP.java
@@ -493,6 +493,59 @@ public class DP {
         }
         return word1.length()- dp[word1.length()][word2.length()] + word2.length()- dp[word1.length()][word2.length()];
     }
+    // lc 1092. Shortest Common Supersequence
+    // find the Longest Common Subsequence (LCS), The LCS represents characters that appear in both strings in the same order.
+    // Then, I construct the Shortest Common Supersequence by:
+    // Starting from the end of both strings and working backwards
+    // If the characters from both strings match, I include it only once
+    // If they differ, I include characters from both strings
+    // Finally, I add any remaining characters from either string
+    // Reverse the constructed string to get the final answer.
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int n=str1.length(),m=str2.length();
+        int dp[][]=new int[n+1][m+1];
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(str1.charAt(i-1)==str2.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1]+1;
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        StringBuilder ans=new StringBuilder();
+        int i=n,j=m;
+        while(i>0 && j>0)
+        {
+            if(str1.charAt(i-1)==str2.charAt(j-1))
+            {
+                ans.append(str1.charAt(i-1));
+                i--;j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1])
+            {
+                ans.append(str1.charAt(i-1));
+                i--;
+            }
+            else
+            {
+                ans.append(str2.charAt(j-1));
+                j--;
+            }
+        }
+        while(i>0){
+            ans.append(str1.charAt(i-1));
+            i--;
+        }
+        while(j>0){
+            ans.append(str2.charAt(j-1));
+            j--;
+        }
+
+        return ans.reverse().toString();
+    }
     public static int longestCommonSubstring(String text1, String text2)
     {
         // similar logic as longest common subsequence 
